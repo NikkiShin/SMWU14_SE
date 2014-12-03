aafcd87d97ca7369d36f02a70e590eefb0b3210a
NikkiShin <yoonj01113@gmail.com>
2014-12-03 10:49:31 +0900

    without metadata

---
+++ Library_manager/src/Admin_BookAdd.java
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+
+public class Admin_BookAdd {
+
+}
+=======
+
+public class Admin_BookAdd {
+
+}
+>>>>>>> origin/master
+=======
+
+public class Admin_BookAdd {
+
+}
+>>>>>>> origin/master
---
+++ Library_manager/src/Admin_BookUpdat.java
@@ -0,0 +1,60 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Admin_BookUpdat {
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	Admin_BookUpdat_db db = new Admin_BookUpdat_db();
+	
+	
+	public Admin_BookUpdat() throws IOException{
+		System.out.println("===도서 정보 수정===");
+		System.out.print("ISBN 입력: ");
+		int isbn = Integer.parseInt(in.readLine());
+		db.checkISBN(isbn);
+		
+	}
+}
+=======
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Admin_BookUpdat {
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	Admin_BookUpdat_db db = new Admin_BookUpdat_db();
+	
+	
+	public Admin_BookUpdat() throws IOException{
+		System.out.println("===도서 정보 수정===");
+		System.out.print("ISBN 입력: ");
+		int isbn = Integer.parseInt(in.readLine());
+		db.checkISBN(isbn);
+		
+	}
+}
+>>>>>>> origin/master
+=======
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Admin_BookUpdat {
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+	Admin_BookUpdat_db db = new Admin_BookUpdat_db();
+	
+	
+	public Admin_BookUpdat() throws IOException{
+		System.out.println("===도서 정보 수정===");
+		System.out.print("ISBN 입력: ");
+		int isbn = Integer.parseInt(in.readLine());
+		db.checkISBN(isbn);
+		
+	}
+}
+>>>>>>> origin/master
