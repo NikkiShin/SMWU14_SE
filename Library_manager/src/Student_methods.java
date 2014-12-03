99cfa0a3b1eef431eef63656c04a758fb4353b46
yak416 <kya0416kr@hanmail.net>
2014-12-03 22:20:21 +0900

    1. Admin_BookUpdat_db.java 코드수정 -도서 정보 중에 수정할 항목 번호를 선택한다. -항목 번호를 선택하면 기존 도서 정보가 나타난다. -그 다음 수정할 도서정보를 텍스트로 입력한다. 2. Admin_BookDelete.java 코드추가 -삭제할 도서의 ISBN을 입력하면 도서의 모든 정보가 출력됨 -사서는 1을 입력하여 해당 도서 정보를 삭제 3. Student_methods.java 코드추가 -대여정보조회, 도서 검색에 필요한 메소드 구현

--- Library_manager/src/Student_methods.java
+++ Library_manager/src/Student_methods.java
@@ -1,4 +1,8 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Student_methods {
 	private Connection dc;
